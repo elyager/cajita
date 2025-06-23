3a96c19916504990a9aa7b248b12dd227bd3b952
Erik Elyager <elyager@gmail.com>
2025-06-22 23:38:04 -0700

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -33,7 +33,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Camera',
+          title: 'CAMARA!',
           tabBarIcon: ({ size, color }) => (
             <Camera size={size + 4} color={color} />
           ),
