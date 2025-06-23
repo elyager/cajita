b50ae5c4aa1d464f3a41d8f13bd9dbaa94f79ef2
Erik Elyager <elyager@gmail.com>
2025-06-22 23:34:03 -0700

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -114,6 +114,7 @@ export default function CameraScreen() {
       <CameraView
         ref={cameraRef}
         className="flex-1"
+
         facing={facing}
         flash={flash}
         mode="picture"
