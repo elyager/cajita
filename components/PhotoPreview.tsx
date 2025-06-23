818543cfb0660cf8be9ccee62fcc8a24f69a82cc
Erik Elyager <elyager@gmail.com>
2025-06-22 23:35:32 -0700

    Updated PhotoPreview.tsx

--- components/PhotoPreview.tsx
+++ components/PhotoPreview.tsx
@@ -112,13 +112,7 @@ export function PhotoPreview({ photoUri, onRetake, onSave }: PhotoPreviewProps) 
       </View>
 
       {/* Bottom Controls */}
-      <View style={{
-        position: 'absolute',
-        bottom: Platform.OS === 'ios' ? 120 : 100,
-        left: 0,
-        right: 0,
-        paddingHorizontal: 20,
-      }}>
+      <View>
         <View className="flex-row justify-around items-center">
           {/* Retake Button */}
           <TouchableOpacity
